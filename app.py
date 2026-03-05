4915f148914f20bc7eb5aebcfa9ea95d3703d5d9
georgevrwtsos2002-rgb <georgevrwtsos2002@gmail.com>
2026-03-05 13:01:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("My First Streamlit App")
+st.title("⚽ Football Shot Map Dashboard")
 
-name = st.text_input("What's your name?")
+uploaded_file = st.file_uploader("Upload shots CSV", type=["csv"])
 
-if name:
-    st.write("Hello", name)
+if uploaded_file:
+
+    df = pd.read_csv(uploaded_file)
+
+    st.write("Data preview")
+    st.dataframe(df)
+
+    fig, ax = plt.subplots()
+
+    goals = df[df["result"]=="Goal"]
+    others = df[df["result"]!="Goal"]
+
+    ax.scatter(others["x"], others["y"], alpha=0.6)
+    ax.scatter(goals["x"], goals["y"], marker="*", s=200)
+
+    ax.set_xlim(0,100)
+    ax.set_ylim(0,100)
+
+    ax.set_title("Shot Map")
+
+    st.pyplot(fig)
